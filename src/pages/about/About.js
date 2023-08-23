d89fbfaab75c7a51aae161bc199640ba92c6da15
affaqkhan211 <affaqkhan211@gmail.com>
2023-08-23 10:51:17 -0700

    projects and about us

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import "./About.css"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="container">
+      <h1 className="white-color text-center mt-5 mb-5">About Us</h1>
+      <div className="about-card">
+
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-12 text-center">
+            <h2 className='white-color'>On a mission to environmental
+              sustainability with NFT's</h2>
+            <p className='white-color'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam vel ea beatae neque reiciendis sint Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo minima repellendus sapiente aliquid cum itaque.</p>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12 text-center">
+            <img src="/images/metaverse5.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <h1 className="text-center white-color mt-5 mb-5">NFT Gen Marketplace with everything for everyone</h1>
+      <div className="text-center">
+        <video className="background-video" type="video/mp4" autoPlay muted loop style={{ width: "500px" }}>
+          <source src="/images/about.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
   )
 }
 
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,8 +1,115 @@
 import React from 'react'
+import "./Projects.css"
+import { projects } from '../../data'
 
 const Projects = () => {
   return (
-    <div className='white-color'>Projects</div>
+    <>
+      <div className="container mt-5">
+        <div className="project-top-div">
+
+        </div>
+
+        <div className="row">
+          <div className="col-lg-3 col-md-3 col-sm-12 text-center">
+            <img src="/images/location.jpg" className='mt-4' alt="" style={{width : "150px", height : "180px"}} />
+            <h3 className="white-color mt-3">Qlab</h3>
+            <p className='white-color'>@qlab</p>
+            <p className='white-color'>Followers 1M+</p>
+
+            <div className="projects-btns d-flex flex-column">
+              <button className="btn-navbar btn mt-4 mt-4">Ongoing Activities</button>
+              <button className="btn-navbar btn mt-4 mt-2">Testimonials</button>
+              <button className="btn-navbar btn mt-4 mt-4">Our Previous Projects</button>
+              <button className="btn-navbar btn mt-4 mt-4">Case Studies</button>
+            </div>
+          </div>
+
+          <div className="col-lg-9 col-md-9 col-sm-12">
+          <div className="row">
+            {
+              projects.map((project) => (
+                <div className="col-lg-3 col-md-3 col-sm-6 col-6 text-center mt-4">
+                <img src={project.img} className="projects-img" alt="" />
+                <p className="white-color mt-2 mb-2">{project.name}</p>
+                <button className="btn-navbar btn mt-2 mb-2" style={{width : "120px"}}>{project.button}</button>
+                </div>
+              ))
+            }
+            </div>
+          </div>
+
+        </div>
+        <div className="project-top-div mt-5">
+
+        </div>
+
+        <div className="row">
+          <div className="col-lg-3 col-md-3 col-sm-12 text-center">
+            <img src="/images/location.jpg" className='mt-4' alt="" style={{width : "150px", height : "180px"}} />
+            <h3 className="white-color mt-3">Qlab</h3>
+            <p className='white-color'>@qlab</p>
+            <p className='white-color'>Followers 1M+</p>
+
+            <div className="projects-btns d-flex flex-column">
+              <button className="btn-navbar btn mt-4 mt-4">Ongoing Activities</button>
+              <button className="btn-navbar btn mt-4 mt-2">Testimonials</button>
+              <button className="btn-navbar btn mt-4 mt-4">Our Previous Projects</button>
+              <button className="btn-navbar btn mt-4 mt-4">Case Studies</button>
+            </div>
+          </div>
+
+          <div className="col-lg-9 col-md-9 col-sm-12">
+          <div className="row">
+            {
+              projects.map((project) => (
+                <div className="col-lg-3 col-md-3 col-sm-6 col-6 text-center mt-4">
+                <img src={project.img} className="projects-img" alt="" />
+                <p className="white-color mt-2 mb-2">{project.name}</p>
+                <button className="btn-navbar btn mt-2 mb-2" style={{width : "120px"}}>{project.button}</button>
+                </div>
+              ))
+            }
+            </div>
+          </div>
+
+        </div>
+        <div className="project-top-div mt-5">
+
+        </div>
+
+        <div className="row">
+          <div className="col-lg-3 col-md-3 col-sm-12 text-center">
+            <img src="/images/location.jpg" className='mt-4' alt="" style={{width : "150px", height : "180px"}} />
+            <h3 className="white-color mt-3">Qlab</h3>
+            <p className='white-color'>@qlab</p>
+            <p className='white-color'>Followers 1M+</p>
+
+            <div className="projects-btns d-flex flex-column">
+              <button className="btn-navbar btn mt-4 mt-4">Ongoing Activities</button>
+              <button className="btn-navbar btn mt-4 mt-2">Testimonials</button>
+              <button className="btn-navbar btn mt-4 mt-4">Our Previous Projects</button>
+              <button className="btn-navbar btn mt-4 mt-4">Case Studies</button>
+            </div>
+          </div>
+
+          <div className="col-lg-9 col-md-9 col-sm-12">
+          <div className="row">
+            {
+              projects.map((project) => (
+                <div className="col-lg-3 col-md-3 col-sm-6 col-6 text-center mt-4">
+                <img src={project.img} className="projects-img" alt="" />
+                <p className="white-color mt-2 mb-2">{project.name}</p>
+                <button className="btn-navbar btn mt-2 mb-2" style={{width : "120px"}}>{project.button}</button>
+                </div>
+              ))
+            }
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
